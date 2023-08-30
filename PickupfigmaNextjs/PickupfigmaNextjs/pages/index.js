6ebe8df568bc72abe8f0e1d80e261cd68ee034d9
Mahima Agarwal <112007998+mahima263@users.noreply.github.com>
2023-08-30 23:02:37 +0530

    Add files via upload

---
+++ PickupfigmaNextjs/PickupfigmaNextjs/pages/index.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import styles from './index.module.css';
+import '../assets';
+function LandingPage() {
+  return (
+    <>
+      <header className={styles.desktop11}>
+        <nav className={styles.navbarWrapper}>
+          <div className={styles.navbar}>
+            <div className={styles.pickupParent}>
+              <span className={styles.pickup}>Pickup</span>
+              <span className={styles.forEnterprise}>For Enterprise</span>
+              <span className={styles.deliveryPartners}>Delivery Partners</span>
+              <span className={styles.login}>Login</span>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className={styles.desktop1Child} />
+      <img className={styles.image1Icon} alt="" src="image 1.png" />
+      <div className={styles.desktop1Item} />
+      <div className={styles.div}>.........................</div>
+      <h2 className={styles.ahmedabad}>Ahmedabad</h2>
+      <h3 className={styles.city}>City</h3>
+      <img className={styles.mapMarkerIcon1} alt="" src="map-marker.svg" />
+      <div className={styles.deliveryHai}>Delivery hai?</div>
+      <strong className={styles.hojayega}>#HoJayega!</strong>
+      <div className={styles.serviceCard}>
+        <div className={styles.twoWheelers}>Two Wheelers</div>
+        <div className={styles.serviceCardChild} />
+        <img className={styles.image4Icon} alt="" src="image 4.png" />
+      </div>
+      <div className={styles.serviceCard1}>
+        <div className={styles.twoWheelers}>Trucks</div>
+        <div className={styles.serviceCardChild} />
+        <img className={styles.image4Icon} alt="" src="image 4.png" />
+      </div>
+      <div className={styles.serviceCard2}>
+        <div className={styles.courierService}>
+          <p className={styles.courier}>Courier</p>
+          <p className={styles.courier}>Service</p>
+        </div>
+        <div className={styles.serviceCardChild} />
+        <img className={styles.image4Icon} alt="" src="image 4.png" />
+      </div>
+      <div className={styles.serviceCard3}>
+        <div className={styles.packersAndMoversContainer}>
+          <p className={styles.courier}>Packers and</p>
+          <p className={styles.courier}>Movers</p>
+        </div>
+        <div className={styles.serviceCardChild} />
+        <img className={styles.image4Icon} alt="" src="image 4.png" />
+      </div>
+      <div className={styles.desktop1Inner} />
+      <div className={styles.takes2Mins1}>(takes ~2 mins)</div>
+      <img className={styles.arrowRightIcon1} alt="" src="arrow-right.svg" />
+      <strong className={styles.getAnEstimateContainer}>
+        <p className={styles.courier}>Get an</p>
+        <p className={styles.courier}>Estimate</p>
+      </strong>
+      <div className={styles.movingABillionContainer}>
+        <p className={styles.courier}>Moving a billion dreams ahead, one delivery at a time. Pursuing an</p>
+        <p className={styles.courier}>unsaid promise to deliver anything, anywhere, anytime with the</p>
+        <p className={styles.courier}>collaborative efforts of our driver-partners.</p>
+      </div>
+      <div className={styles.desktop1Child1} />
+      <strong className={styles.b}>19+</strong>
+      <strong className={styles.lakh}>
+        <p className={styles.courier}>5 Lakh+</p>
+      </strong>
+      <strong className={styles.crore}>1 Crore+</strong>
+      <strong className={styles.crore1}>10 Crore+</strong>
+      <strong className={styles.cities}>Cities</strong>
+      <strong className={styles.driverPartners}>Driver Partners</strong>
+      <strong className={styles.customers}>Customers</strong>
+      <strong className={styles.trips}>Trips</strong>
+      <strong className={styles.andThanksToContainer}>
+        <p className={styles.courier}>And thanks to you, we are growing each &amp;</p>
+        <p className={styles.courier}>every day!</p>
+      </strong>
+      <img className={styles.image5Icon} alt="" src="image 5.png" />
+      <strong className={styles.weAreServingContainer}>
+        <p className={styles.courier}>We are serving in Delhi NCR, Bengaluru, Mumbai, Hyderabad, Ahmedabad, Jaipur, Pune, Kolkata, Surat, Chennai, Coimbatore, Indore,</p>
+        <p className={styles.courier}>Nagpur, Chandigarh, Lucknow, Vadodara, Ludhiana, Kochi, and Nashik.</p>
+      </strong>
+      <section className={styles.heading}>
+        <strong className={styles.someWordsFrom}>SOME WORDS FROM OUR HAPPY CUSTOMERS</strong>
+        <img className={styles.image6Icon1} alt="" src="image 6.png" />
+        <img className={styles.image7Icon1} alt="" src="image 7.png" />
+      </section>
+      <div className={styles.activeCard}>
+        <div className={styles.activeCardChild} />
+        <div className={styles.wayBetterThanContainer}>
+          <p className={styles.courier}>“Way better than naaka</p>
+          <p className={styles.courier}>waalas. Have shifted all my</p>
+          <p className={styles.courier}>logistics needs to Porter and I</p>
+          <p className={styles.courier}>can now safely focus on my</p>
+          <p className={styles.courier}>business growth. Amazing</p>
+          <p className={styles.courier}>service!”</p>
+        </div>
+        <div className={styles.nagpur}>Nagpur</div>
+        <strong className={styles.vinay}>Vinay</strong>
+        <img className={styles.image8Icon3} alt="" src="image 8.png" />
+      </div>
+      <div className={styles.unactive}>
+        <div className={styles.unactiveChild} />
+        <div className={styles.wayBetterThanContainer}>
+          <p className={styles.courier}>“Way better than naaka</p>
+          <p className={styles.courier}>waalas. Have shifted all my</p>
+          <p className={styles.courier}>logistics needs to Porter and I</p>
+          <p className={styles.courier}>can now safely focus on my</p>
+          <p className={styles.courier}>business growth. Amazing</p>
+          <p className={styles.courier}>service!”</p>
+        </div>
+        <div className={styles.nagpur}>Nagpur</div>
+        <strong className={styles.vinay}>Vinay</strong>
+        <img className={styles.image8Icon3} alt="" src="image 8.png" />
+      </div>
+      <div className={styles.unactive1}>
+        <div className={styles.unactiveChild} />
+        <div className={styles.wayBetterThanContainer}>
+          <p className={styles.courier}>“Way better than naaka</p>
+          <p className={styles.courier}>waalas. Have shifted all my</p>
+          <p className={styles.courier}>logistics needs to Porter and I</p>
+          <p className={styles.courier}>can now safely focus on my</p>
+          <p className={styles.courier}>business growth. Amazing</p>
+          <p className={styles.courier}>service!”</p>
+        </div>
+        <div className={styles.nagpur}>Nagpur</div>
+        <strong className={styles.vinay}>Vinay</strong>
+        <img className={styles.image8Icon3} alt="" src="image 8.png" />
+      </div>
+      <div className={styles.desktop1Child2} />
+      <div className={styles.accordination}>
+        <img className={styles.chevronDownIcon3} alt="" src="chevron-down.svg" />
+        <div className={styles.whatIsPickup1}>What is Pickup App?</div>
+        <img className={styles.accordinationChild} alt="" src="Vector 1.svg" />
+      </div>
+      <div className={styles.accordination1}>
+        <img className={styles.chevronDownIcon4} alt="" src="chevron-down.svg" />
+        <div className={styles.whatIsPickup1}>How do I use Pickup App?</div>
+        <img className={styles.accordinationItem} alt="" src="Vector 1.svg" />
+      </div>
+      <div className={styles.accordination2}>
+        <img className={styles.chevronDownIcon5} alt="" src="chevron-down.svg" />
+        <div className={styles.howDoIContainer}>
+          <p className={styles.courier}>How do I book a tempo/mini truck/bike or intercity courier services online from the Porter</p>
+          <p className={styles.courier}>app?</p>
+        </div>
+        <img className={styles.accordinationInner} alt="" src="Vector 1.svg" />
+        <div className={styles.getReliableAndContainer}>
+          <p className={styles.courier}>Get reliable and affordable logistics services from the Porter app by following these steps:</p>
+          <ul className={styles.signInToThePorterAppChoo}>
+            <li className={styles.signInTo}>Sign in to the Porter app</li>
+            <li className={styles.signInTo}>Choose the service you want - trucks, 2wheeler or intercity courier services</li>
+            <li className={styles.signInTo}>Set your pick up and drop off location</li>
+            <li className={styles.signInTo}>Browse through the list of available vehicles (Tata Ace, Pickup 8ft, 2wheeler etc) and pick the one that suits your moving needs</li>
+            <li className={styles.signInTo}>Enter a few additional details, apply coupons to get exciting discounts, and confirm your booking.</li>
+            <li>Sit back and relax - our verified driver partners will do the rest.</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.frequentlyAskedQuestions1}>Frequently asked Questions</div>
+      <div className={styles.hereAreSomeContainer}>
+        <p className={styles.courier}>Here are some of the top FAQs to support you. Please do feel free to reach out to us for any</p>
+        <p className={styles.courier}>questions that you might have had but are not answered by the following.</p>
+      </div>
+      <footer className={styles.footer}>
+        <div className={styles.logo}>Pickup</div>
+        <div className={styles.sendAnything}>Send anything,</div>
+        <div className={styles.anywhere}>anywhere,</div>
+        <div className={styles.anytime}>anytime</div>
+        <div className={styles.company}>Company</div>
+        <div className={styles.quickLinks}>Quick Links</div>
+        <div className={styles.support}>Support</div>
+        <img className={styles.image2Icon} alt="" src="image 2.png" />
+        <img className={styles.image3Icon} alt="" src="image 3.png" />
+        <div className={styles.aboutUs}>About Us</div>
+        <div className={styles.cityTempo}>City Tempo</div>
+        <div className={styles.forEnterprise1}>For Enterprise</div>
+        <div className={styles.packersMovers1}>Packers &amp; Movers</div>
+        <div className={styles.courier1}>Courier</div>
+        <div className={styles.services}>Services</div>
+        <div className={styles.offers}>Offers</div>
+        <div className={styles.careers}>Careers</div>
+        <div className={styles.blog}>Blog</div>
+        <div className={styles.contactUs}>Contact Us</div>
+        <div className={styles.privacyPolicy}>Privacy Policy</div>
+        <div className={styles.termsOfService1}>Terms of Service</div>
+        <div className={styles.insuranceFaqs}>Insurance FAQs</div>
+        <div className={styles.driverPartnerTerms1}>Driver Partner Terms &amp; Conditions</div>
+        <strong className={styles.weAreHere1}>We are here</strong>
+        <div className={styles.delhiNcr}>Delhi NCR</div>
+        <div className={styles.hyderabad}>Hyderabad</div>
+        <div className={styles.bangalore}>Bangalore</div>
+        <div className={styles.mumbai}>Mumbai</div>
+        <div className={styles.vadodara}>Vadodara</div>
+        <div className={styles.footerChild} />
+        <div className={styles.instagram}>
+          <img className={styles.vectorIcon5} alt="" src="Vector.svg" />
+        </div>
+        <div className={styles.facebook}>
+          <img className={styles.vectorIcon6} alt="" src="Vector.svg" />
+        </div>
+        <div className={styles.linkedin}>
+          <img className={styles.vectorIcon7} alt="" src="Vector.svg" />
+        </div>
+        <div className={styles.youtube}>
+          <img className={styles.vectorIcon8} alt="" src="Vector.svg" />
+        </div>
+        <div className={styles.twitter}>
+          <img className={styles.vectorIcon9} alt="" src="Vector.svg" />
+        </div>
+        <div className={styles.pickupLogisticsSolutions1}>2023 Pickup Logistics Solutions Pvt. Ltd. | CIN: 0000000000000</div>
+      </footer>
+    </>
+  );
+}
+
+export default LandingPage;
+
